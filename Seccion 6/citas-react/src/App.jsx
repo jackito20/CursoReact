1c30e9e6c36674cbb03fd2f3e52d152e4136ecf0
Jackeline Hernandez <jhernandez@serempre.com>
2023-02-28 07:41:44 -0500

    until 61

--- Seccion 6/citas-react/src/App.jsx
+++ Seccion 6/citas-react/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import ListadoPacientes from './components/ListadoPacientes'
+import Formulario from './components/Formulario'
+import Header from './components/Header'
 
 function App() {
   
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className='container mx-auto mt-20'>        
+      <Header />
+
+      <div className='mt-12 md:flex'>
+        <Formulario />
+        <ListadoPacientes />
+      </div>
     </div>
+  
   )
 }
 
---
+++ Seccion 6/citas-react/src/components/Formulario.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Formulario() {
+  return (
+    <div className='md:w-1/2 lg:w-2/5'>
+      <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
+        <p className='text-lg my-5 text-center'>
+            Añade Pacientes y {''}
+            <span className='text-indigo-600 font-bold'>Administralos</span>
+        </p>
+
+        <form className=' bg-white shadow-md rounded-lg py-10
+         px-5 mb-10'>
+            <div>
+                <label htmlFor='mascota' className='block text-gray-700 uppercase'>Nombre Mascota</label>
+                <input 
+                    id='mascota'
+                    type="text"
+                    placeholder='Nombre de la Mascota'
+                    className=' border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md'
+                />
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Formulario
