cbe32ef9977508214528adc9e656a7f2957c64d8
Jackeline Hernandez <jhernandez@serempre.com>
2023-03-14 06:34:20 -0500

    75

--- Seccion 6/citas-react/src/components/Formulario.jsx
+++ Seccion 6/citas-react/src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react';
+import Error from './Error';
 
 const Formulario = ({pacientes, setPacientes}) => {
   const [nombre, setNombre] = useState('');
@@ -53,11 +54,7 @@ const Formulario = ({pacientes, setPacientes}) => {
           className=' bg-white shadow-md rounded-lg py-10
           px-5 mb-10 margin-botton-10'>
             
-            { error && 
-              <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-                <p>Todos los campos son obligatorios</p>
-              </div>
-            }  
+            { error && <Error mensaje = {'Todos los campos son obligatorios'}/>}  
             
             <div className='mb-5'>
                 <label htmlFor='mascota' className='block text-gray-700 uppercase'>Nombre Mascota</label>
