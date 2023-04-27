71bc7cce95887a1c41097529bcc47406414370f4
Jackeline Hernandez <jhernandez@serempre.com>
2023-04-27 16:17:58 -0500

    .

---
+++ Seccion 2/FundamentosJS/js/28.js
@@ -0,0 +1,7 @@
+// Imports y Exports
+//import comoquieras from "./funciones.js"          //Cuando se usa export default (se puede importar con cualquier nombre)
+import sumar from "./funciones.js"
+
+const resultado = sumar(20, 30)
+
+console.log(resultado)
---
+++ Seccion 2/FundamentosJS/js/funciones.js
@@ -0,0 +1,14 @@
+function sumar(numero1, numero2){
+    return numero1 + numero2
+}
+
+function restar(numero1, numero2){
+    return numero1 - numero2
+}
+
+export default sumar      //Solo funciona para una funcion
+
+/*export {
+    sumar, 
+    restar
+}*/
